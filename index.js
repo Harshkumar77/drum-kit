a0ca0f43bc2aab5f758b002178b70ff0e6e28627
mighty_volcano <harshkumar@noreply.com>
2021-10-01 16:32:24 +0530

    added images and sounds to card

---
+++ index.js
@@ -0,0 +1,39 @@
+var btns = document.querySelectorAll(".drum");
+
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function () {
+        var key = this.innerHTML;
+        console.log(key);
+        switch (key) {
+            case 'w':
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case 'a':
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case 's':
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case 'd':
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            case 'j':
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+            case 'k':
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+            case 'l':
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+        }
+    });
+}
+console.log(2);
